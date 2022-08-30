6a5c6eb5a746dd5e26dc7482a78a59d1f7680469
André Reis <arrr.cin@gmail.com>
2022-08-29 20:11:16 -0400

    Add a simple page to show product details

---
+++ lib/src/screens/product_details.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductDetails extends StatelessWidget {
+  final Product product;
+  const ProductDetails({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(product.toString()),
+      ),
+    );
+  }
+}
--- lib/src/screens/product_list.dart
+++ lib/src/screens/product_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:product_db_mobile/src/screens/product_details.dart';
 import 'package:product_db_mobile/src/screens/product_item.dart';
 import '../providers/product_provider.dart';
 
@@ -9,23 +10,37 @@ class ProductList extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final products = ref.watch(productsProvider);
-    return Expanded(
-        child: products.when(
-      data: (data) => ListView.builder(
-        padding: const EdgeInsets.only(top: 0.0),
-        itemCount: data.length,
-        itemBuilder: (context, index) => ProviderScope(
-          child: ProductItem(
-            product: data[index],
-          ),
-        ),
-      ),
+    return products.when(
+      data: (data) => data.isNotEmpty
+          ? ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 0.0),
+              itemCount: data.length,
+              itemBuilder: (context, index) => ProviderScope(
+                child: InkWell(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ProductDetails(product: data[index]),
+                    ),
+                  ),
+                  child: ProductItem(
+                    product: data[index],
+                  ),
+                ),
+              ),
+            )
+          : const Center(
+              child: Text('There is no data to show.'),
+            ),
       loading: () => const Center(
         child: CircularProgressIndicator(),
       ),
       error: (error, e) => Center(
-        child: Text(error.toString()),
+        child: Center(
+          child: Text(error.toString()),
+        ),
       ),
-    ));
+    );
   }
 }
